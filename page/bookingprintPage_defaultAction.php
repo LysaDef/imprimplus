cab1fe8c5edc4dba46ecf5e1b7fce7d7f80b4c84
Mathieu <CHARRO10Ma@lycee-ndduroc.com>
2021-01-05 14:02:47 +0100

    cadeau

--- page/bookingprintPage_defaultAction.php
+++ page/bookingprintPage_defaultAction.php
@@ -12,16 +12,22 @@
             echo $html;
             ?>
         </select>
+        <br>
+        <br>
         <caption>choisir l'image</caption>
         <div class="input-group mb-3">
             <label class="input-group-text" for="inputGroupFile01">Upload</label>
             <input type="file" class="form-control" id="inputGroupFile01">
         </div>
+        <br>
         <form class="form-floating">
             <input type="email" class="form-control" id="floatingInputValue" placeholder="quantitée" value="">
         </form>
+        <br>
         <div class="input-group mb-3">
             <button class="btn btn-outline-secondary" type="button" id="button-addon1">Validé</button>
         </div>
+    <br>
+    <h1> Liste de vos commandes </h1>
 
 <?php include('template/footer.php'); ?>
