1253caebfe4306c522e836e6234edca392a955a3
Mathieu <CHARRO10Ma@lycee-ndduroc.com>
2021-01-05 10:26:47 +0100

    progression bookingprintPage_Default

--- page/bookingprintPage_defaultAction.php
+++ page/bookingprintPage_defaultAction.php
@@ -1,7 +1,31 @@
 <?php include('template/header.php'); ?>
+
+    
     <div class="container">
-
-
-
+        
+        <div class="row g-2">
+            <div class="col-md">
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInputGrid" placeholder="name@example.com" value="mdo@example.com">
+                    <label for="floatingInputGrid">Email address</label>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="form-floating">
+                    <select class="form-select" id="floatingSelectGrid" aria-label="Floating label select example">
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                    <label for="floatingSelectGrid">Works with selects</label>
+                </div>
+            </div>
+        </div>
+        //upload image
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="inputGroupFile01">Upload</label>
+            <input type="file" class="form-control" id="inputGroupFile01">
+        </div>
     </div>
 <?php include('template/footer.php'); ?>
