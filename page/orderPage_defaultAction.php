ed2fb62753b436a873ec70099141cf0a33fbb0f9
Lysa <DEFURN29Ly@lycee-ndduroc.com>
2021-01-05 09:31:17 +0100

    début du tableau

--- page/orderPage_defaultAction.php
+++ page/orderPage_defaultAction.php
@@ -1,7 +1,17 @@
 <?php include('template/header.php'); ?>
     <div class="container">
 
+<table>
+    <caption>Les commandes mois par mois, client par client</caption>
+    <thead>
+    <tr><th>#</th><th>Nom</th><th>Prenom</th><th>Date</th><th>Quantité</th><th>Modèle</th><th>Image</th></tr>
+    </thead>
+    <tbody>
 
+    </tbody>
+    <tfoot>
+    <tr><th>#</th><th>Nom</th><th>Prenom</th><th>Date</th><th>Quantité</th><th>Modèle</th><th>Image</th></tr>
+</table>
     
     </div>
 <?php include('template/footer.php'); ?>
