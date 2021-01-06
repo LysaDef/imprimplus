9d4d79a517b127724f8437268944197d635e4ffb
Mathieu <CHARRO10Ma@lycee-ndduroc.com>
2021-01-06 09:15:56 +0100

    .

--- page/turnoverPage_defaultAction.php
+++ page/turnoverPage_defaultAction.php
@@ -15,10 +15,6 @@
 
 <?php 
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> de142b0c6bd1801390fb68f44c5205bf365cc76b
 //$totalCa= "";
 //$totalCa.='<tr>';
 //$totalCa.='<td>'.$caClient[0]['caMoisDernier'].'</th>';
@@ -26,24 +22,19 @@
 //$totalCa.='<td>'.$caClient[2]['caMoisDernier'].'</th>';
 //$totalCa.='<td>'.$caClient[3]['caMoisDernier'].'</th>';
 //$totalCa.='</tr>';
-<<<<<<< HEAD
-=======
-=======
+
 $totalCa='';
 $totalCa.=$caClient;
 var_dump($caClient['caMoisDernier']);
->>>>>>> e74df138d923d872cfceea269f8a962e71d45215
->>>>>>> de142b0c6bd1801390fb68f44c5205bf365cc76b
 
 foreach ($caClient as $ca){
     $totalCa.='<tr>';
     $totalCa.='<td>'.'<p>'.$ca['caMoisDernier'].'<p>'.'</th>';
 }
-<<<<<<< HEAD
+
 $totalCa.=$totalCa .$ca[0]['caMoisDernier'].$ca[1]['caMoisDernier'].$ca[2]['caMoisDernier'].$ca[3]['caMoisDernier'];
-=======
 $totalCa.=$totalCa + $ca['caMoisDernier'];
->>>>>>> de142b0c6bd1801390fb68f44c5205bf365cc76b
+
 
 ?>
 
