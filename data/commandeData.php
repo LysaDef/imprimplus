2eac91e8eea24cede0892241ad0feeb6c925151b
Mathieu <CHARRO10Ma@lycee-ndduroc.com>
2021-01-06 09:57:14 +0100

    commandedataresponsible

--- data/commandeData.php
+++ data/commandeData.php
@@ -31,22 +31,11 @@ function commandePersoData_getAll(){
 
 }
 
-<<<<<<< HEAD
 function commandeForResponsibleData_getAll()
 {
     $request = "SELECT commander.id AS 'id', modele.libelle AS 'libelle', user.nom AS 'nom', user.prenom AS 'prenom', commander.quantite AS 'quantite', commander.date AS 'date' FROM commander JOIN salarie ON commander.salarie_id=salarie.id JOIN modele ON commander.modele_id=modele.id JOIN user ON salarie.user_id=user.id ORDER BY salarie_id ASC";
     $results = Connection::query($request);
     return $results;
     
-=======
-
-function commandePersoData_sendBDD($datas){
-
-    $request = "INSERT INTO commander VALUES(NULL,'".$datas['modele']."', '".$datas['salarieid']."', '".$datas['quantity']."', 'Undifinded', NOW())";
-    /*$request = "INSERT INTO test VALUES('".$datas['modele']."', '".$datas['quantity']."')";*/
-    $results=Connection::exec($request);
-    return $results;
-
->>>>>>> 7e2af2c70b1881fb828ca485e1181d05413a54eb
 
 }
