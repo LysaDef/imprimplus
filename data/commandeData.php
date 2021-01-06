05facf05d5821a0fa083f23afb8371b3b77f0238
Mathieu <CHARRO10Ma@lycee-ndduroc.com>
2021-01-06 10:45:18 +0100

    responsibleforclientpage avancement

--- data/commandeData.php
+++ data/commandeData.php
@@ -31,27 +31,11 @@ function commandePersoData_getAll(){
 
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 5276588a0c01b7a31221f1ce4ad983b658e1eb26
 function commandeForResponsibleData_getAll()
 {
     $request = "SELECT commander.id AS 'id', modele.libelle AS 'libelle', user.nom AS 'nom', user.prenom AS 'prenom', commander.quantite AS 'quantite', commander.date AS 'date' FROM commander JOIN salarie ON commander.salarie_id=salarie.id JOIN modele ON commander.modele_id=modele.id JOIN user ON salarie.user_id=user.id ORDER BY salarie_id ASC";
     $results = Connection::query($request);
     return $results;
-<<<<<<< HEAD
     
 
-=======
-
-}
-
-function commandePersoData_sendBDD($datas){
-
-    $request = "INSERT INTO commander VALUES(NULL,'".$datas['modele']."', '".$datas['salarieid']."', '".$datas['quantity']."', 'Undifinded', NOW())";
-
-    $results=Connection::exec($request);
-    return $results;
->>>>>>> 5276588a0c01b7a31221f1ce4ad983b658e1eb26
 }
