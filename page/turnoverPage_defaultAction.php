609f01002eb70f77fa735de6226755c61acd789f
Lysa <DEFURN29Ly@lycee-ndduroc.com>
2021-01-06 09:06:36 +0100

    Ca

--- page/turnoverPage_defaultAction.php
+++ page/turnoverPage_defaultAction.php
@@ -4,15 +4,37 @@
 
 <h3>Chiffre d'affaire total généré mois par mois par toutes les commandes</h3>
 
+<table class="table">
+  <thead class="thead-light">
+    <tr>
+
+      <th scope="col">Chiffre d'affaire par client</th>
+    </tr>
+  </thead>
+  <tbody>
+
 <?php 
 
-$totalCa='';
-$totalCa.=$caClient['caMoisDernier'];
-var_dump($caClient['caMoisDernier']);
+//$totalCa= "";
+//$totalCa.='<tr>';
+//$totalCa.='<td>'.$caClient[0]['caMoisDernier'].'</th>';
+//$totalCa.='<td>'.$caClient[1]['caMoisDernier'].'</th>';
+//$totalCa.='<td>'.$caClient[2]['caMoisDernier'].'</th>';
+//$totalCa.='<td>'.$caClient[3]['caMoisDernier'].'</th>';
+//$totalCa.='</tr>';
 
-//$totalCa=$caClient['caMoisDernier'];?>
+foreach ($caClient as $ca){
+    $totalCa.='<tr>';
+    $totalCa.='<td>'.'<p>'.$ca['caMoisDernier'].'<p>'.'</th>';
+}
+$totalCa.=$totalCa + $ca['caMoisDernier'];
+
+?>
 
 <?php echo $totalCa ?>
+
+</tbody>
+</table>
 
     </div>
 
