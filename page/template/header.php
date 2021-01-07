93136b9a2ef3006547eb9ec78208855bb867e084
Nathan <GUILLE16Na@lycee-ndduroc.com>
2021-01-07 15:48:49 +0100

    SITE:LOGO

--- page/template/header.php
+++ page/template/header.php
@@ -4,6 +4,7 @@
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+        <link rel="icon" href="http://127.0.0.1/prj/imprimplus/page/img/viewers/header_logo.png" />
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 
