e92f1c0c6e413a2716534a78cfd500168e846567
Nathan <GUILLE16Na@lycee-ndduroc.com>
2021-01-08 08:23:18 +0100

    Version finale du site

--- config/env.php
+++ config/env.php
@@ -6,7 +6,7 @@ $ENV =[
     'APP_VERSION' => 'v0',
     /* Paramètres pour l'accès à la base de données */
     'DB_HOST'     => 'localhost',
-    'DB_DATABASE' => 'imprimplus_v0',
+    'DB_DATABASE' => 'imprimplus_v3',
     'DB_USERNAME' => 'usersio',
     'DB_PASSWORD' => 'pwsio'
 ];
